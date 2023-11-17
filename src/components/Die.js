01418d2a757ae86a46cb75164b7fa7b6b0078983
Yekta Turan <r.yekt.93@gmail.com>
2023-11-17 12:07:46 +0100

    Styled die component and main

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,14 +2,30 @@
 import { css } from "@emotion/react";
 
 function Die(props) {
-  return <div css={style}>{props.value}</div>;
+  return (
+    <div css={style}>
+      <div>{props.value}</div>
+    </div>
+  );
 }
 
 const style = css`
-  min-height: calc((80vh - 2rem) / 2);
   display: flex;
   justify-content: center;
   align-items: center;
+
+  > div {
+    height: 5rem;
+    width: 5rem;
+    font-size: 2rem;
+    background-color: white;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.15);
+    border-radius: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
 `;
 
 export default Die;
